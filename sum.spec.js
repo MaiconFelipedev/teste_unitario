1d88154947649b8b02b17cb8ee1dd7ba39097299
Maicon Felipe da Silva Souza <20212370020@ad.ifpb.edu.br>
2023-10-20 14:35:37 -0300

    first test ok

--- sum.spec.js
+++ sum.spec.js
@@ -1,1 +1,8 @@
 const sum = require('./sum');
+
+//recebe uma string e uma função de callback
+test('somar dois números', () => {
+    const resultado = sum(1,2)
+    expect(resultado).toBe(3)
+
+}) 
